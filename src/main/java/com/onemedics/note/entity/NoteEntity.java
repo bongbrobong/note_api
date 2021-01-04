8cd5c041a28df93bb571e4c649e4c48d00e0a738
김봉우 <bwkim@onemedics.net>
2021-01-04 15:49:02 +0900

    [토이 프로젝트]  - initial commit

---
+++ src/main/java/com/onemedics/note/entity/NoteEntity.java
@@ -0,0 +1,24 @@
+package com.onemedics.note.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "note")
+public class NoteEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "note_no", nullable = false)
+    private long noteNo;
+
+    @Column(name = "menu_no", nullable = false)
+    private long menuNo;
+
+    @Column(name = "note_ver")
+    private String noteVer;
+
+    @Column(name = "note_cont")
+    private String noteVer;
+}
